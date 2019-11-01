9445d1091b36c035e282b2be6c51ef9fe6c31d6d
Peter Jang <peter.x.jang@gmail.com>
2019-10-31 21:23:34 -0500

    Add complete dictionary solution

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -7,4 +7,31 @@ input_word = gets.chomp
 
 response = HTTP.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=___")
 definitions = response.parse
-p definitions
+
+response = HTTP.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/topExample?useCanonical=false&api_key=___")
+top_example = response.parse
+
+response = HTTP.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/pronunciations?useCanonical=false&limit=50&api_key=___")
+pronunciations = response.parse
+
+puts "DEFINITIONS:"
+index = 0
+definitions.length.times do
+  definition = definitions[index]
+  puts "#{index + 1}. #{definition["text"]}"
+  puts
+  index = index + 1
+end
+puts
+
+puts "TOP EXAMPLE"
+puts top_example["text"]
+puts
+
+puts "PRONUNCIATIONS"
+index = 0
+pronunciations.length.times do
+  pronunciation = pronunciations[index]
+  puts "#{index + 1}. #{pronunciation["raw"]}"
+  index = index + 1
+end
